c072b85512f5e88481dca79dbc2707c10152f1e7
Huseyn Mikayil <huseyn-mikayil@mail.ru>
2020-05-12 14:11:03 +0400

    auth context was created

--- client/src/App.js
+++ client/src/App.js
@@ -6,28 +6,27 @@ import Trend from './components/layout/Trend';
 import Profile from './components/pages/Profile';
 
 import PostState from './context/post/PostState';
+import AuthState from './context/auth/AuthState';
 
 import './App.css';
 
 const App = () => {
 
-  // componentDidMount() {
-  //   this.getPosts();
-  // }
-
   return (
-    <PostState>
-      <Router>
-        <div className="App">
-          <Header />
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/profile' component={Profile} />
-          </Switch>
-          <Trend />
-        </div>
-      </Router>
-    </PostState>
+    <AuthState>
+      <PostState>
+        <Router>
+          <div className="App">
+            <Header />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/profile' component={Profile} />
+            </Switch>
+            <Trend />
+          </div>
+        </Router>
+      </PostState>
+    </AuthState>
   );
 }
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,8 +2,30 @@ import React from 'react'
 
 const Register = () => {
     return (
-        <div>
-
+        <div class="auth">
+            <div class="form">
+                <i class="fab fa-twitter"></i>
+                <h2>Create your account</h2>
+                <form>
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" />
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" />
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" />
+                    </div>
+                    <div class="form-group">
+                        <label for="password2">Confirm Password</label>
+                        <input type="password" name="password2" />
+                    </div>
+                    <input type="submit" value="Log in" class="btn btn-link btn-l" />
+                </form>
+            </div>
         </div>
     )
 }
